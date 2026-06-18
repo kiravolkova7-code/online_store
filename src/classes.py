eea113cf75bd6c71565aa46b377cd314e2d9a0b2
august <kira_volkova_7@mail.ru>
2026-06-18 14:35:17 +0300

    classes complited

--- src/classes.py
+++ src/classes.py
@@ -4,9 +4,25 @@ class Product():
     price = int
     quantity = int
 
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
 
 
 class Category():
     name = str
     description = str
     products = str
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
