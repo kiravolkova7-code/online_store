920059ad81ef445ef2fdd1fad7beb5513fb23518
august <kira_volkova_7@mail.ru>
2026-06-19 20:25:06 +0300

    add new func

--- src/classes.py
+++ src/classes.py
@@ -1,7 +1,7 @@
 class Product():
     name : str
     description : str
-    price : float
+    __price : float
     quantity : int
 
     def __init__(self, name, description, price, quantity):
@@ -10,19 +10,75 @@ class Product():
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, product_data: dict):
+        """
+        Класс-метод для создания объекта Product из словаря.
+        """
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
+
+    @property
+    def price(self) -> float:
+        """Возвращает значение приватного атрибута цены."""
+        return self.__price
+
+
+    @price.setter
+    def price(self, value: float):
+        """
+        Устанавливает цену товара.
+        Если цена <= 0, выводит предупреждение и не меняет текущую цену.
+        """
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
 
 class Category():
     name : str
     description : str
-    products : list
+    __products: list
 
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
 
         Category.category_count += 1
-        Category.product_count += len(products)
+        if products is not None:
+            for prod in products:
+                self.add_product(prod)
+
+    def add_product(self, product):
+        """
+        Добавляет товар в категорию.
+        """
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise ValueError("Можно добавить только объект класса Product")
+
+    @property
+    def products(self):
+        """Геттер для получения списка товаров в категории"""
+        return self.__products
+
+
+    def get_products(self):
+        """
+        Возвращает список товаров в виде отформатированных строк.
+        """
+        formatted_list = []
+        for product in self.__products:
+            line = f"{product.name}, {product.price:.2f} руб. Остаток: {product.quantity} шт."
+            formatted_list.append(line)
+        return formatted_list
